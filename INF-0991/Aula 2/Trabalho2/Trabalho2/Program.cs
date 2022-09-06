be711f11bb0f4bb3dc66151f9917e3465c2571e0
Victor Akira Hassuda Silva <victorakira@hotmail.com>
2022-09-06 00:55:31 -0300

    Resolvendo bug v3.2

--- INF-0991/Aula 2/Trabalho2/Trabalho2/Program.cs
+++ INF-0991/Aula 2/Trabalho2/Trabalho2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!"); //Bug encontrado v3.2
 
 //Adicionando v1.0
 //Adicionando v2.0
